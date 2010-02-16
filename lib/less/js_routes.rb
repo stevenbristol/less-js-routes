4d584ecc96ce2eb5b00c54d61f40c071c6deb7b9
Alvaro Bautista <alvarobp@gmail.com>
2010-02-16 23:09:16 +0100

    Wrapped generated javascript functions in LessJsRoutes object

--- lib/less/js_routes.rb
+++ lib/less/js_routes.rb
@@ -34,7 +34,7 @@ module Less
         route.conditions[:path_info].captures.each do |cap|
           if cap.is_a?(Rack::Mount::GeneratableRegexp::DynamicSegment)
             segg = cap.name.to_s.gsub(':', '')
-            s << "#{segg} = less_check_parameter(#{segg});"
+            s << "#{segg} = LessJsRoutes.less_check_parameter(#{segg});"
           end
         end
         s
@@ -56,73 +56,75 @@ module Less
       def get_params others = ''
         x = ''
         x += " + " unless x.blank? || others.blank?
-        x += "less_get_params(#{others})" unless others.blank?
+        x += "LessJsRoutes.less_get_params(#{others})" unless others.blank?
         x
       end
 
       def get_js_helpers
         <<-JS
-function less_json_eval(json){return eval('(' +  json + ')')}  
+  less_json_eval: function(json){return eval('(' +  json + ')');},
 
-function jq_defined(){return typeof(jQuery) != "undefined"}
+  jq_defined: function(){return typeof(jQuery) != "undefined";},
 
-function less_get_params(obj){
-  #{'console.log("less_get_params(" + obj + ")");' if @@debug} 
-  if (jq_defined()) { return obj }
-  if (obj == null) {return '';}
-  var s = [];
-  for (prop in obj){
-    s.push(prop + "=" + obj[prop]);
-  }
-  return s.join('&') + '';
-}
+  less_get_params: function(obj){
+    #{'console.log("LessJsRoutes.less_get_params(" + obj + ")");' if @@debug} 
+    if (jq_defined()) { return obj; }
+    if (obj == null) {return '';}
+    var s = [];
+    for (prop in obj){
+      s.push(prop + "=" + obj[prop]);
+    }
+    return s.join('&') + '';
+  },
 
-function less_merge_objects(a, b){
-  #{'console.log("less_merge_objects(" + a + ", " + b + ")");' if @@debug} 
-  if (b == null) {return a;}
-  z = new Object;
-  for (prop in a){z[prop] = a[prop]}
-  for (prop in b){z[prop] = b[prop]}
-  return z;
-}
+  less_merge_objects: function(a, b){
+    #{'console.log("LessJsRoutes.less_merge_objects(" + a + ", " + b + ")");' if @@debug}
+    if (b == null) {return a;}
+    z = new Object;
+    for (prop in a){z[prop] = a[prop];}
+    for (prop in b){z[prop] = b[prop];}
+    return z;
+  },
 
-function less_ajax(url, verb, params, options){
-  #{'console.log("less_ajax(" + url + ", " + verb + ", " + params +", " + options + ")");' if @@debug} 
-  if (verb == undefined) {verb = 'get';}
-  var res;
-  if (jq_defined()){
-    v = verb.toLowerCase() == 'get' ? 'GET' : 'POST'
-    if (verb.toLowerCase() == 'get' || verb.toLowerCase() == 'post'){p = less_get_params(params);}
-    else{p = less_get_params(less_merge_objects({'_method': verb.toLowerCase()}, params))} 
-    #{'console.log("less_merge_objects:v : " + v);' if @@debug} 
-    #{'console.log("less_merge_objects:p : " + p);' if @@debug} 
-    res = jQuery.ajax(less_merge_objects({async:false, url: url, type: v, data: p}, options)).responseText;
-  } else {  
-    new Ajax.Request(url, less_merge_objects({asynchronous: false, method: verb, parameters: less_get_params(params), onComplete: function(r){res = r.responseText;}}, options));
-  }
-  if (url.indexOf('.json') == url.length-5){ return less_json_eval(res);}
-  else {return res;}
-}
-function less_ajaxx(url, verb, params, options){
-  #{'console.log("less_ajax(" + url + ", " + verb + ", " + params +", " + options + ")");' if @@debug} 
-  if (verb == undefined) {verb = 'get';}
-  if (jq_defined()){
-    v = verb.toLowerCase() == 'get' ? 'GET' : 'POST'
-    if (verb.toLowerCase() == 'get' || verb.toLowerCase() == 'post'){p = less_get_params(params);}
-    else{p = less_get_params(less_merge_objects({'_method': verb.toLowerCase()}, params))} 
-    #{'console.log("less_merge_objects:v : " + v);' if @@debug} 
-    #{'console.log("less_merge_objects:p : " + p);' if @@debug} 
-    jQuery.ajax(less_merge_objects({ url: url, type: v, data: p, complete: function(r){eval(r.responseText)}}, options));
-  } else {  
-    new Ajax.Request(url, less_merge_objects({method: verb, parameters: less_get_params(params), onComplete: function(r){eval(r.responseText);}}, options));
-  }
-}
-function less_check_parameter(param) {
-  if (param === undefined) {
-    param = '';
-  }
-  return param;
-}
+  less_ajax: function(url, verb, params, options){
+    #{'console.log("LessJsRoutes.less_ajax(" + url + ", " + verb + ", " + params +", " + options + ")");' if @@debug} 
+    if (verb == undefined) {verb = 'get';}
+    var res;
+    if (jq_defined()){
+      v = verb.toLowerCase() == 'get' ? 'GET' : 'POST';
+      if (verb.toLowerCase() == 'get' || verb.toLowerCase() == 'post'){p = LessJsRoutes.less_get_params(params);}
+      else{p = LessJsRoutes.less_get_params(LessJsRoutes.less_merge_objects({'_method': verb.toLowerCase()}, params));} 
+      #{'console.log("LessJsRoutes.less_merge_objects:v : " + v);' if @@debug} 
+      #{'console.log("LessJsRoutes.less_merge_objects:p : " + p);' if @@debug} 
+      res = jQuery.ajax(LessJsRoutes.less_merge_objects({async:false, url: url, type: v, data: p}, options)).responseText;
+    } else {  
+      new Ajax.Request(url, LessJsRoutes.less_merge_objects({asynchronous: false, method: verb, parameters: LessJsRoutes.less_get_params(params), onComplete: function(r){res = r.responseText;}}, options));
+    }
+    if (url.indexOf('.json') == url.length-5){ return LessJsRoutes.less_json_eval(res);}
+    else {return res;}
+  },
+
+  less_ajaxx: function(url, verb, params, options){
+    #{'console.log("less_ajax(" + url + ", " + verb + ", " + params +", " + options + ")");' if @@debug} 
+    if (verb == undefined) {verb = 'get';}
+    if (jq_defined()){
+      v = verb.toLowerCase() == 'get' ? 'GET' : 'POST';
+      if (verb.toLowerCase() == 'get' || verb.toLowerCase() == 'post'){p = LessJsRoutes.less_get_params(params);}
+      else{p = less_get_params(LessJsRoutes.less_merge_objects({'_method': verb.toLowerCase()}, params));}
+      #{'console.log("LessJsRoutes.less_merge_objects:v : " + v);' if @@debug} 
+      #{'console.log("LessJsRoutes.less_merge_objects:p : " + p);' if @@debug} 
+      jQuery.ajax(LessJsRoutes.less_merge_objects({ url: url, type: v, data: p, complete: function(r){eval(r.responseText)}}, options));
+    } else {  
+      new Ajax.Request(url, LessJsRoutes.less_merge_objects({method: verb, parameters: LessJsRoutes.less_get_params(params), onComplete: function(r){eval(r.responseText);}}, options));
+    }
+  },
+
+  less_check_parameter: function(param) {
+    if (param === undefined) {
+      param = '';
+    }
+    return param;
+  },
 JS
       end
 
@@ -130,25 +132,28 @@ JS
 
 
       def generate!
-        s = get_js_helpers
+        s = "LessJsRoutes = {\n\n"
+        s << get_js_helpers
         
-        ActionController::Routing::Routes.routes.each do |route|
-          if name = route.name
-  # s << build_path( route.segments)
-  # s << "\n"
-  # s << route.inspect# if route.instance_variable_get(:@conditions)[:method] == :put
+        ActionController::Routing::Routes.named_routes.routes.each do |name, route|
+          # s << build_path( route.segments)
+          # s << "\n"
+          # s << route.inspect# if route.instance_variable_get(:@conditions)[:method] == :put
             
-            s << "/////\n//#{route}\n" if @@debug
+          s << "/////\n//#{route}\n" if @@debug
             
-            s << <<-JS
-  function #{name}_path(#{build_params route}){ #{build_default_params route} return '#{build_path route}';}
-  function #{name}_ajax(#{build_params route, 'params'}, options){ #{build_default_params route} return less_ajax('#{build_path route}', verb, params, options);}
-  function #{name}_ajaxx(#{build_params route, 'params'}, options){ #{build_default_params route} return less_ajaxx('#{build_path route}', verb, params, options);}
+          s << <<-JS
+  
+  // #{route.name} => #{route.path}
+  #{name.to_s}_path: function(#{build_params route}){ #{build_default_params route} return '#{build_path route}';},
+  #{name.to_s}_ajax: function(#{build_params route, 'params'}, options){ #{build_default_params route} return LessJsRoutes.less_ajax('#{build_path route}', verb, params, options);},
+  #{name.to_s}_ajaxx: function(#{build_params route, 'params'}, options){ #{build_default_params route} return LessJsRoutes.less_ajaxx('#{build_path route}', verb, params, options);},
   JS
-          end
-          File.open("#{Rails.public_path}/javascripts/less_routes.js", 'w') do |f|
-            f.write s
-          end
+        end
+        s << "\n}"
+        
+        File.open("#{Rails.public_path}/javascripts/less_routes.js", 'w') do |f|
+          f.write s
         end
       end
 
